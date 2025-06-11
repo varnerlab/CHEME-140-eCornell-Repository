0627d203b6e22395c9bcc1bf9c8045c089746a70
Jeffrey Varner <jdv27@cornell.edu>
2025-06-11 12:13:51 -0400

    141 M2 project initial impl has been completed
    impl completed

--- courses/CHEME-141/module-2/Include.jl
+++ courses/CHEME-141/module-2/Include.jl
@@ -7,6 +7,7 @@ const _PATH_TO_SOUNDS = joinpath(_ROOT, "sounds");
 # if we are missing any packages, install them -
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
@@ -14,10 +15,14 @@ end
 # Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 
 # load external package -
+using VLDataScienceMachineLearningPackage
 using JLD2
 using FileIO
 using DataFrames
 using CSV
+using Distributions
+using Statistics
+using UnicodePlots
 
 # include my codes -
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
