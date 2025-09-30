01e71778fd43e07149576d576c4fa5f158924e70
Jeffrey D Varner <jdv27@cornell.edu>
2025-09-30 14:13:04 -0400

    Add Urea cycle FBA notebook and related files
    Added CHEME-144-M2-UreaCycle-FBA-Primal-Ungraded-Codio-Activity.ipynb and supporting files for flux balance analysis of the urea cycle, including Julia source files, data, and figures. Removed two older module 2 notebooks. Updated Project.toml and reorganized module 3 notebook. This update introduces a new FBA workflow and supporting code for CHEME-144 module 2.

---
+++ courses/CHEME-144/module-2/Include-FBA.jl
@@ -0,0 +1,47 @@
+# setup paths -
+const _ROOT = @__DIR__;
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# if we are missing any packages, install them -
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using LinearAlgebra
+using BenchmarkTools
+using Statistics
+using Test
+using Images
+using TestImages
+using ImageMagick
+using ImageIO
+using DelimitedFiles
+using Plots
+using DataFrames
+using Random
+using Distributions
+using PrettyTables
+
+# load external packages
+using JLD2
+using CSV
+using FileIO
+using JuMP
+using GLPK
+
+# setup random number generator -
+Random.seed!(1234); # seed the random number generator for reproducibility
+
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Parser.jl"));
+include(joinpath(_PATH_TO_SRC, "Network.jl"));
+include(joinpath(_PATH_TO_SRC, "Handler.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Stoichiometric.jl"));
