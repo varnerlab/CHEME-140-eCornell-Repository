d81579962727a2d5a6d92767089468e1446a4f77
Jeffrey D Varner <jdv27@cornell.edu>
2025-10-30 14:11:40 -0400

    145 M2
    Rock Paper Scissors

--- courses/CHEME-145/module-2/Include.jl
+++ courses/CHEME-145/module-2/Include.jl
@@ -5,6 +5,7 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 # check: do we need to download any packages?
 using Pkg
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add("IJulia");
     Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
@@ -19,6 +20,3 @@ using LinearAlgebra
 using Statistics
 using DataFrames
 using PrettyTables
-
-# load my codes -
-# ...
